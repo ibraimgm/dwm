607c3eab4bac33e8e59a2ab1f38c6cfa7622e48a
Rafael Ibraim <ibraim.gm@gmail.com>
2019-12-15 22:13:32 -0300

    Added screen capture key

--- config.def.h
+++ config.def.h
@@ -67,9 +67,11 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
 static const char *lockcmd[]  = {"/bin/sh", "-c", "$HOME/.config/lock.sh", NULL };
+static const char *flameshotcmd[] = {"flameshot", "gui", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
+     	{ 0,                            XK_Print,  spawn,          {.v = flameshotcmd } },
 	{ MODKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ ALTKEY,                       XK_space,  spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
